c15d224525db925aab3d1c583c87ee26a224b95c
전혜미@아이브랩 <hmjeon@aiblab.co.kr>
2024-01-12 11:29:19 +0900

    Firestore로 데이터 생성하기: 게시글 폼 작업

--- src/components/posts/PostForm.tsx
+++ src/components/posts/PostForm.tsx
@@ -1,30 +1,65 @@
+import { useContext, useState } from 'react';
 import { FiImage } from 'react-icons/fi';
-
-const handleFileUpload = () => {
-	//
-};
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from 'firebaseApp';
+import { toast } from 'react-toastify';
+import AuthContext from 'context/AuthContext';
 
 export default function PostForm() {
+	const [content, setContent] = useState<string>('');
+	const { user } = useContext(AuthContext);
+
+	const handleFileUpload = () => {
+		//
+	};
+
+	const onSubmit = async (e: any) => {
+		e.preventDefault();
+
+		try {
+			await addDoc(collection(db, 'posts'), {
+				content: content,
+				createAt: new Date()?.toLocaleDateString('ko', {
+					hour: '2-digit',
+					minute: '2-digit',
+					second: '2-digit',
+				}),
+				uid: user?.uid,
+				email: user?.email,
+			});
+			setContent('');
+			toast.success('게시글을 생성했습니다.');
+		} catch (e: any) {
+			// console.log(e);
+		}
+	};
+
+	const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+		const {
+			target: { name, value },
+		} = e;
+
+		if (name === 'content') {
+			setContent(value);
+		}
+	};
+
 	return (
-		<form className="post-form">
+		<form className="post-form" onSubmit={onSubmit}>
 			<textarea
 				name="content"
 				id="content"
 				className="post-form__textarea"
 				placeholder="What is happening?"
+				onChange={onChange}
+				value={content}
 				required
 			/>
 			<div className="post-form__submit-area">
 				<label htmlFor="file-input" className="post-form__file">
 					<FiImage className="post-form__file-icon" />
 				</label>
-				<input
-					type="file"
-					name="file-input"
-					className="hidden"
-					accept="image/*"
-					onChange={handleFileUpload}
-				/>
+				<input type="file" name="file-input" className="hidden" accept="image/*" onChange={handleFileUpload} />
 				<input type="submit" value="Tweet" className="post-form__submit-btn" />
 			</div>
 		</form>
